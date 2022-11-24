2137c3f736f44a3e2479699be38024f902a89b21
Mario López González <mariolopezgonzalez2001@gmail.com>
2022-11-24 19:55:34 +0100

    done

--- P3/recetas/models.py
+++ P3/recetas/models.py
@@ -1,10 +1,19 @@
 # Create your models here.
 # recetas/models.py
+import re
 from django.db import models
+from django.core.exceptions import ValidationError
 import os
+
+def first_letter_capital(value):
+    if not (re.match(pattern="([A-Z])", string=value[0])):
+        raise ValidationError(
+            (value + ' debe empezar por mayúscula'),
+            params={'value': value},
+        )
   
 class Receta(models.Model):
-  nombre       = models.CharField(max_length=200)
+  nombre       = models.CharField(max_length=200, validators=[first_letter_capital])
   preparacion  = models.TextField(max_length=5000)
   
   def __str__(self):
--- P3/recetas/views.py
+++ P3/recetas/views.py
@@ -10,40 +10,37 @@ from .forms import RecetaForm, IngredienteForm, FotoForm
 from .models import Ingrediente, Receta, Foto
 from django.contrib import messages
 
-# Create your views here.
-@csrf_exempt
-def index(request):
+def get_mode(request):
     mode = ''
     if request.session.__contains__('dark_mode'):
         mode = request.session.__getitem__('dark_mode')
+
+    return mode
+
+# Create your views here.
+@csrf_exempt
+def index(request):
+    
     context = {
         'saludado': 'Pepito',
-        'modo': mode
+        'modo': get_mode(request)
     }
     return render(request, 'base.html', context)
 
 @csrf_exempt
 def busqueda(request):
-    mode = ''
-    if request.session.__contains__('dark_mode'):
-        mode = request.session.__getitem__('dark_mode')
-
     name = request.GET.get("search_input")
     recetas = Receta.objects.filter(nombre__icontains = name)
     
     context = {
         'recetas': recetas,
-        'modo': mode
+        'modo': get_mode(request)
     }
     
     return render(request, 'busqueda.html', context)
 
 @csrf_exempt
 def detalle(request, _id):
-    mode = ''
-    if request.session.__contains__('dark_mode'):
-        mode = request.session.__getitem__('dark_mode')
-
     recetas = Receta.objects.filter(id = _id)
     ingredientes = Ingrediente.objects.filter(receta_id = _id)
     fotos = Foto.objects.filter(receta_id = _id)
@@ -52,7 +49,7 @@ def detalle(request, _id):
         'receta': recetas[0],
         'ingredientes': ingredientes,
         'fotos': fotos,
-        'modo': mode  
+        'modo': get_mode(request)
     }
     
     return render(request, 'detalle.html', context)
@@ -85,42 +82,23 @@ def recetas_delete(request, _id):
 @csrf_exempt
 def add_receta(request):
 
-    mode = ''
-    if request.session.__contains__('dark_mode'):
-        mode = request.session.__getitem__('dark_mode')
+    formReceta = RecetaForm(request.POST or None)
 
-    if request.method == "POST":
-        formReceta = RecetaForm(request.POST)
-
-        if formReceta.is_valid():
-            post = formReceta.save()
-
-            context = {
-                'receta': post,
-                'modo': mode  
-            }
-            
-            messages.add_message(request, messages.SUCCESS, 'Receta ' + post.nombre + ' añadida correctamente')
-            return render(request, 'detalle.html', context)
-
-    formReceta = RecetaForm
+    if formReceta.is_valid():
+        receta = formReceta.save()
+        messages.add_message(request, messages.SUCCESS, 'Receta ' + receta.nombre + ' añadida correctamente')
+        return redirect('detalle', _id = receta.id)
 
     context = {
         'formReceta': formReceta,
-        'modo': mode  
+        'modo': get_mode(request)  
     }
 
     return render(request, 'add_receta.html', context)
 
 @csrf_exempt
 def receta_edit(request, _id):
-
     receta = Receta.objects.get(id = _id)
-
-    mode = ''
-    if request.session.__contains__('dark_mode'):
-        mode = request.session.__getitem__('dark_mode')
-
     formReceta = RecetaForm(instance = receta)
 
     if request.method == 'POST':
@@ -134,7 +112,7 @@ def receta_edit(request, _id):
     context = {
         'formReceta': formReceta,
         'receta': receta,
-        'modo': mode  
+        'modo': get_mode(request)  
     }
 
     return render(request, 'add_receta.html', context)
